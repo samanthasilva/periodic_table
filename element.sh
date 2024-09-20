b9ad68bf78a51857816ac51d43ebf757a9f30178
Samantha Silva <samanthinhasilva@gmail.com>
2024-09-20 13:50:05 +0000

    corrected_message

--- element.sh
+++ element.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-if [[ $1 ]]
- then
- if [[ ! $1 =~ ^[0-9]+$ ]]
- then
- ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius,boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.name LIKE '$1%' ORDER BY atomic_number LIMIT 1")
- else
- ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius,boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.atomic_number=$1")
- fi
-   if [[ -z $ELEMENT ]]
-   then 
-     echo "I could not find that element in the database."
-   else
-    echo $ELEMENT | while IFS=\| read ATOMIC_NUMBER ATOMIC_MASS MPC BPC SYM NAME TYPE
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    # Argument is a number
+    QUERY="SELECT e.atomic_number, e.symbol, e.name, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius 
+           FROM elements e 
+           JOIN properties p ON e.atomic_number=p.atomic_number 
+           JOIN types t ON p.type_id=t.type_id 
+           WHERE e.atomic_number = $1"
+  else
+    # Argument is a string (symbol or name)
+    QUERY="SELECT e.atomic_number, e.symbol, e.name, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius 
+           FROM elements e 
+           JOIN properties p ON e.atomic_number=p.atomic_number 
+           JOIN types t ON p.type_id=t.type_id 
+           WHERE e.symbol = '$1' OR e.name = '$1'"
+  fi
+
+  ELEMENT=$($PSQL "$QUERY")
+
+  if [[ -z $ELEMENT ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    echo "$ELEMENT" | while IFS="|" read ATOMIC_NUMBER SYMBOL NAME TYPE ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS
     do
-      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYM). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MPC celsius and a boiling point of $BPC celsius."
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
     done
-   fi 
-   else
- echo "Please provide an element as an argument."
+  fi
 fi
